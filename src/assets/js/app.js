801f842e450bb264eae8d62a0a1180ce68454191
Ali.t.nazari <91375198+Silent-Watcher@users.noreply.github.com>
2022-08-07 20:35:47 +0430

    add boxshadow ui page with bunch of errors :)

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,7 +1,8 @@
 'use strict';
-let $ = document;
-const menu = $.querySelector('#menu_list');
+// let $ = document;
+const menu = document.querySelector('#menu_list');
 const light = menu.nextElementSibling;
+const sliders = document.querySelectorAll('.rangeSliders');
 
 function siblings(element) {
   let siblings = [];
@@ -10,13 +11,31 @@ function siblings(element) {
   });
   return siblings;
 }
-
+// navbar animation
 [...menu.children].forEach((menuItem) => {
   menuItem.addEventListener('click', function () {
     light.style.left = `${menuItem.offsetLeft + light.offsetWidth / 3}px`;
     menuItem.firstElementChild.classList.add('active', 'text-primary');
     siblings(menuItem).forEach((sibling) => {
-        sibling.firstElementChild.classList.remove('active','text-primary');
+      sibling.firstElementChild.classList.remove('active', 'text-primary');
     });
   });
 });
+//
+// slider value
+sliders.forEach((slider) => {
+  slider.addEventListener('input', function () {
+    let sliderNavigator =
+      this.parentElement.previousElementSibling.firstElementChild;
+    sliderNavigator.textContent = +this.value;
+    sliderNavigator.style.left = `${this.value/2}%`;
+    sliderNavigator.classList.add('show');
+  });
+  slider.addEventListener('blur', function () {
+    let sliderNavigator =
+      this.parentElement.previousElementSibling.firstElementChild;
+    sliderNavigator.classList.remove('show');
+  });
+});
+// color picker
+const colorPicker = new iro.ColorPicker('#color_picker');
