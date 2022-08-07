31812cb7e41f57536afac5a4adaf0dd1488bddea
Ali.t.nazari <91375198+Silent-Watcher@users.noreply.github.com>
2022-08-07 17:36:11 +0430

    bug fix #1

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -12,11 +12,9 @@ function siblings(element) {
 }
 
 [...menu.children].forEach((menuItem) => {
-  //   console.log(menuItem);
   menuItem.addEventListener('click', function () {
     light.style.left = `${menuItem.offsetLeft + light.offsetWidth / 3}px`;
     menuItem.firstElementChild.classList.add('active', 'text-primary');
-    // console.log(siblings(menuItem));
     siblings(menuItem).forEach((sibling) => {
         sibling.firstElementChild.classList.remove('active','text-primary');
     });
