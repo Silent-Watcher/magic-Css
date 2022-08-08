3622da154db06d60c4f433b3961186644dcc6bee
Ali.t.nazari <91375198+Silent-Watcher@users.noreply.github.com>
2022-08-08 23:23:33 +0430

    add copy to clipboard btn for bxsh result

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,54 +1,14 @@
 'use strict';
+// bxsh stands for box-shadow
 let $ = document;
 const menu = $.querySelector('#menu_list');
 const light = menu.nextElementSibling;
 const sliders = $.querySelectorAll('.rangeSliders');
 const bxshBox = $.querySelector('#boxShadowMagic_sample');
+const bxshResultDisplay = $.querySelector('#bxsh_result_display');
+const bxshResultBtn = bxshResultDisplay.lastElementChild;
 let [xBxsh, yBxsh, blurBxsh, spreadBxsh, colorBxsh] = [0, 0, 0, 0, '#5e81ec'];
-
-bxshBox.style.boxShadow = `${xBxsh}px ${yBxsh}px ${blurBxsh}px ${spreadBxsh}px ${colorBxsh}`;
-
-function getRGB(rgbString) {
-  rgbString = rgbString.replace('rgb(', '');
-  rgbString = rgbString.replace(')', '');
-  rgbString = rgbString.trim();
-  return rgbString.split(', ');
-}
-// console.log(getRGB('rgb(255, 209, 130)'));
-
-function siblings(element) {
-  let siblings = [];
-  [...element.parentElement.children].forEach((sibling) => {
-    if (sibling !== element) siblings.push(sibling);
-  });
-  return siblings;
-}
-// navbar animation
-[...menu.children].forEach((menuItem) => {
-  menuItem.addEventListener('click', function () {
-    light.style.left = `${menuItem.offsetLeft + light.offsetWidth / 3}px`;
-    menuItem.firstElementChild.classList.add('active', 'text-primary');
-    siblings(menuItem).forEach((sibling) => {
-      sibling.firstElementChild.classList.remove('active', 'text-primary');
-    });
-  });
-});
-//
-// slider value
-let rangeSliders = $.querySelectorAll('.slider_range_input');
-rangeSliders.forEach((rangeSlider) => {
-  rangeSlider.addEventListener('input', function () {
-    this.value = +this.value;
-    let navigator = this.previousElementSibling;
-    navigator.textContent = this.value;
-    navigator.classList.add('show');
-  });
-  rangeSlider.addEventListener('blur', function () {
-    this.previousElementSibling.classList.remove('show');
-  });
-});
-
-// color picker for magic box shadow
+// color picker initialization for magic box shadow
 let boxShadowSliderPicker = new iro.ColorPicker(
   '#color_boxShadow_color_picker',
   {
@@ -85,6 +45,63 @@ let boxShadowSliderPicker = new iro.ColorPicker(
   }
 );
 //
+// initial alert
+const Toast = Swal.mixin({
+  toast: true,
+  position: 'top-end',
+  showConfirmButton: false,
+  timer: 3000,
+  timerProgressBar: true,
+  didOpen: (toast) => {
+    toast.addEventListener('mouseenter', Swal.stopTimer);
+    toast.addEventListener('mouseleave', Swal.resumeTimer);
+  },
+});
+// 
+function getRGB(rgbString) {
+  rgbString = rgbString.replace('rgb(', '');
+  rgbString = rgbString.replace(')', '');
+  rgbString = rgbString.trim();
+  return rgbString.split(', ');
+}
+function siblings(element) {
+  let siblings = [];
+  [...element.parentElement.children].forEach((sibling) => {
+    if (sibling !== element) siblings.push(sibling);
+  });
+  return siblings;
+}
+// initial highlight.js
+hljs.highlightAll();
+//
+// Initial quantification for bxsh result
+bxshBox.style.boxShadow = `${xBxsh}px ${yBxsh}px ${blurBxsh}px ${spreadBxsh}px ${colorBxsh}`;
+bxshResultDisplay.firstElementChild.firstElementChild.innerHTML =
+  bxshBox.style.boxShadow;
+//
+// navbar animation
+[...menu.children].forEach((menuItem) => {
+  menuItem.addEventListener('click', function () {
+    light.style.left = `${menuItem.offsetLeft + light.offsetWidth / 3}px`;
+    menuItem.firstElementChild.classList.add('active', 'text-primary');
+    siblings(menuItem).forEach((sibling) => {
+      sibling.firstElementChild.classList.remove('active', 'text-primary');
+    });
+  });
+});
+// slider value animation
+let rangeSliders = $.querySelectorAll('.slider_range_input');
+rangeSliders.forEach((rangeSlider) => {
+  rangeSlider.addEventListener('input', function () {
+    this.value = +this.value;
+    let navigator = this.previousElementSibling;
+    navigator.textContent = this.value;
+    navigator.classList.add('show_range_slider');
+  });
+  rangeSlider.addEventListener('blur', function () {
+    this.previousElementSibling.classList.remove('show_range_slider');
+  });
+});
 // range slide value change detect
 let bxshSliderRanges = document.querySelectorAll('.bxsh_slider_range');
 bxshSliderRanges.forEach((bxshSliderRange) => {
@@ -107,6 +124,9 @@ bxshSliderRanges.forEach((bxshSliderRange) => {
         spreadBxsh = this.value;
         bxshBox.style.boxShadow = `${xBxsh}px ${yBxsh}px ${blurBxsh}px ${this.value}px #000`;
       }
+      bxshResultDisplay.firstElementChild.firstElementChild.innerHTML =
+        bxshBox.style.boxShadow;
+      hljs.highlightAll();
     }
   );
 });
@@ -118,8 +138,10 @@ boxShadowSliderPicker.on(['color:init', 'color:change'], function (color) {
   $.querySelector('#bxsh_r').value = getRGB(color.rgbString)[0];
   $.querySelector('#bxsh_g').value = getRGB(color.rgbString)[1];
   $.querySelector('#bxsh_b').value = getRGB(color.rgbString)[2];
-  // colorBxsh = color.hexString;
   bxshBox.style.boxShadow = `${xBxsh}px ${yBxsh}px ${blurBxsh}px ${spreadBxsh}px ${color.hexString}`;
+  bxshResultDisplay.firstElementChild.firstElementChild.innerHTML =
+    bxshBox.style.boxShadow;
+  hljs.highlightAll();
 });
 [...$.querySelector('#bxsh_colorPalette').children].forEach(
   (bxshColorPalette) => {
@@ -128,13 +150,43 @@ boxShadowSliderPicker.on(['color:init', 'color:change'], function (color) {
     });
   }
 );
-//
-// $.querySelector('#bxsh_inset_input').addEventListener('input', function () {
-//   if (this.checked) {
-//     inset = true;
-//     bxshBox.style.boxShadow = `${inset ? 'inset' :''}`+ bxshBox.style.boxShadow;
-//   } else {
-//     inset = false;
-//     bxshBox.style.boxShadow = bxshBox.style.boxShadow.replace('inset','');
-//   }
-// });
+// ------------------ copy bxsh result to the clipboard and show some stuff like tooltip and alert------------
+const tooltip = new bootstrap.Tooltip(bxshResultDisplay.lastElementChild, {
+  title: 'copy to clipboard',
+  customClass: 'custom-tooltip',
+  placement: 'top',
+});
+// appear copyToClipBoard btn
+bxshResultDisplay.addEventListener('mouseenter', function () {
+  this.lastElementChild.style.transform = 'scale(1)';
+});
+bxshResultDisplay.addEventListener('mouseleave', function () {
+  this.lastElementChild.style.transform = 'scale(0)';
+});
+// copu to clipboard
+bxshResultBtn.addEventListener('click', function () {
+  Toast.fire({
+    icon: 'success',
+    title: 'copied to clipboard 🤗',
+  });
+  this.style.backgroundColor = '#7ed321';
+  this.firstElementChild.classList.replace('fa-clipboard-list', 'fa-check');
+  navigator.clipboard.writeText(
+    this.previousElementSibling.firstElementChild.textContent
+  );
+  tooltip.setContent({ '.tooltip-inner': 'copied !' });
+  setTimeout(() => {
+    this.style.backgroundColor = '#4169e1';
+    this.firstElementChild.classList.replace('fa-check', 'fa-clipboard-list');
+    tooltip.setContent({ '.tooltip-inner': 'copy to clip board' });
+  }, 500);
+});
+// show tooltip
+bxshResultBtn.addEventListener('mouseenter', () => {
+  tooltip.enable();
+});
+// hide tooltip
+bxshResultBtn.addEventListener('mouseleave', () => {
+  tooltip.hide();
+});
+// ----------------------------------------------------------
